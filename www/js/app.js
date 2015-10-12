c5d197f8bb9d8e613a16abace8aede28790ea3b4
Alexander A. Ramírez M <alexander.ramirez@synergy-gb.com>
2015-10-12 13:52:58 -0430

    Add new ion-nav-view referenced at app.js in the new state

--- www/js/app.js
+++ www/js/app.js
@@ -21,9 +21,17 @@ angular.module('starter', ['ionic'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('tabs', {
-      url: '/tabs',
+      url: '/tab',
       abstract: true,
       templateUrl: 'templates/tabs.html' 
+    })
+    .state('tabs.home', {
+      url: '/home',
+      views: {
+        'home-tab': {
+          templateUrl: 'templates/home.html'
+        }
+      }
     });
 
   $urlRouterProvider.otherwise("/tab/home");
